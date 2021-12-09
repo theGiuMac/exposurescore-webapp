7fead3ade5347e71efcb050a2db5868aa5a9afdf
Giulio Maccari <g.maccari@student.rug.nl>
2021-12-09 18:06:11 +0100

    Fixed times_seen UPDATE

--- calculate.php
+++ calculate.php
@@ -30,7 +30,8 @@ if ($resultCheckRow->num_rows > 0) { // this row is founded in the database
         $new_date = date("Y-m-d h:m:s");
         $sqlLastSeen = "UPDATE agentstrings SET last_seen ='" . $new_date . "' WHERE browserid =" . $rowCheck['browserid'];
         $conn->query($sqlLastSeen);
-        $sqlTimesSeen = "UPDATE agentstrings SET times_seen ='" . $rowCheck['times_seen'] + 1 ."' WHERE browserid =" . $rowCheck['browserid'];
+        $new_times_seen = $rowCheck['times_seen'] + 1;
+        $sqlTimesSeen = "UPDATE agentstrings SET times_seen ='" . $new_times_seen ."' WHERE browserid =" . $rowCheck['browserid'];
         $conn->query($sqlTimesSeen);
     }
 } else if ($resultCheckRow2->num_rows > 0) { // intermediate case 1 cvss_score is zero
