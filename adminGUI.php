9769b5b0b0a1c930d2d0411c27b4fee1c7cda386
Giulio Maccari <g.maccari@student.rug.nl>
2021-12-17 14:35:10 +0100

    Put everything into one file

--- adminGUI.php
+++ adminGUI.php
@@ -170,19 +170,19 @@
                 if (isset($_POST['update'])) {
                     $attributes_pass = require("./update_Num_Nulls.php");
                     if ($attributes_pass == True) {
-                        $unnormalized_pass = require("./update_unnormalized_scores.php");
-                        if ($unnormalized_pass == True) {
-                            $normalized_pass = require("./update_normalized_scores.php");
-                            if ($normalized_pass == True) {
-                                echo "<pre>All relative scores were update successfully</pre>";
-                            } else {
-                                echo "<pre>There was a problem with updating the normalized scores</pre>";
-                            }
-                        } else {
-                            echo "<pre>There was a problem with updating the unnormalized scores</pre>";
-                        }
+                        //$unnormalized_pass = require("./update_unnormalized_scores.php");
+                        //if ($unnormalized_pass == True) {
+                        //    $normalized_pass = require("./update_normalized_scores.php");
+                        //    if ($normalized_pass == True) {
+                        echo "<pre>All relative scores were update successfully</pre>";
+                        //    } else {
+                        //        echo "<pre>There was a problem with updating the normalized scores</pre>";
+//                            }
+                        //} else {
+                        //    echo "<pre>There was a problem with updating the unnormalized scores</pre>";
+                        //}
                     } else {
-                        echo "<pre>There was a problem with updating the attributes</pre>";
+                        echo "<pre>There was a problem with updating the scores!</pre>";
                     }
                 }
                 ?>
--- update_Num_Nulls.php
+++ update_Num_Nulls.php
@@ -242,4 +242,92 @@ for ($idx=0; $idx < 47; $idx++) {
 $num_rows_query = "SELECT COUNT(*) FROM agentstrings";
 $num_rows_tot = $conn->query($num_rows_query);
 
+// ----------------------- UNNORMALIZED SECTION ------------------------
 
+echo "<pre> Arrives here </pre>";
+
+$done = 0;
+while ($done < $size) {
+
+	$row = $all_rows_result->fetch_array()
+
+	set_time_limit(120);
+
+	$attributes = "SELECT * FROM attributes";
+	$result_att = $conn->query($attributes);
+	$score = 0;
+	$num_of_exposed_attributes = $row['num_of_exposed_attributes'];
+	if ($result_att->num_rows > 0) {
+	    $count = 0;
+	    while ($rowatt = $result_att->fetch_assoc()) {
+	    //while($count != 
+	        if ($row[$count] == -1 || $row[$count] == "unknown" || $row[$count] === NULL) {
+	            $score += 0;
+	        } else {
+	            $score += (($rowatt["sofj"] + $rowatt["sj"]) * (($rowatt["rjovern"] * ($num_of_exposed_attributes / 47)) + $rowatt["vj"]));
+	        }
+	        $count++;
+	    }
+	}
+
+	$score = round($score, 3);
+
+	$update_unnormalized_query = "UPDATE agentstrings SET unnormalized_score = '" . $score . "' WHERE browserid= '" . $row['browserid'] . "'";
+	$update_result = $conn->query($update_unnormalized_query);
+
+	$done++;
+}
+
+echo "<pre> Unnormalized are done </pre>";
+
+// ------------------- NORMALIZED SECTION ---------------------
+
+// Normalize over the [1-10] range
+function normalize($min, $max, $score) {
+	$normalized = ($score - $min) / ($max - $min) * 10;
+	return round($normalized, 2);
+}
+
+// Query that updates the normalize score of a particular browser
+function update_normalized($conn, $score, $browserid) {
+	$score_update_query = "UPDATE agentstrings SET new_privacy_score = '" . $score . "' WHERE browserid = '" . $browserid . "'";
+	$score_update_result = $conn->query($score_update_query);
+
+	$time_privacy_score = date("Y-m-d h:m:s");
+	$time_update_query = "UPDATE agentstrings SET time_privacy_score = '" . $time_privacy_score . "' WHERE browserid = '" . $browserid . "'";
+	$time_update_result = $conn->query($time_update_query);
+}
+
+// Find minimum and maximum for normalizing the relative scores
+$min_max_query = "SELECT MAX(unnormalized_score) AS max, MIN(unnormalized_score) AS min FROM agentstrings";
+$min_max_result = $conn->query($min_max_query);
+
+$min_max = $min_max_result->fetch_array();
+$min = $min_max['min'];
+$max = $min_max['max'];
+
+$all_rows_query = "SELECT * FROM agentstrings";
+$all_rows_result = $conn->query($all_rows_query);
+
+$totalRows = $all_rows_result->num_rows;
+
+// ------------- Main update loop -----------------
+
+// For each broswer, normalize and update its relative score
+for ($count=0; $count < $totalRows; $count++) {
+
+	// Allow 1 minute for each computation
+	set_time_limit(60);
+
+	// Get each browser's row
+	$row = $all_rows_result->fetch_array();
+
+	// Get each browser's unnormalized score
+	$unnormalized_score = $row['unnormalized_score'];
+
+	// Normalize the score
+	$normalized_score = normalize($min, $max, $unnormalized_score);
+
+	// Update the normalized score (`new_privacy_score`)
+	update_normalized($conn, $normalized_score, $row['browserid']);
+}
