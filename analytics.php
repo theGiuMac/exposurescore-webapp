41b13f55497ebe8cf0aac7be17f4c7b2a59fbb9e
Giulio <maccarigiulio@pm.me>
2022-06-22 17:59:55 +0200

    Added src from jsDelivr for jQuery and modified ajax request

--- analytics.php
+++ analytics.php
@@ -2,21 +2,23 @@
 <html>
   <head>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.8.0/dist/chart.min.js" integrity="sha256-cHVO4dqZfamRhWD7s4iXyaXWVK10odD+qp4xidFzqTI=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
   </head>
 
   <body>
     <script language='javascript'>
     window.addEventListener('load', function() {
-        $.ajax({
+        jQuery.ajax({
             type: 'POST',
-            url: 'api.php',
-            datatype: 'json',
-            success: function (result) {
+            url: 'https://mybrowserscore.com/api.php',
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (response) {
                 var ctx = document.getElementById("chart").getContext("2d");
                 var mychart = new Chart(ctx,
                                         {
                                             type: 'bar',
-                                            data: JSON.parse(result),
+                                            data: JSON.parse(response),
                                             options: {
                                                 scales: {
                                                     yAxes: [{
@@ -28,8 +30,8 @@
                                             }
                                         })
             }
-            error: function(jqXHR, textStatus, errorThrown) {
-                alert('error');
+            error: function(response) {
+                console.log(response);
             }
         })
     })
