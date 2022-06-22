2836f9eea01a6bafcefa4784a342fd2aa0358eaf
Giulio <maccarigiulio@pm.me>
2022-06-22 12:25:16 +0200

    Added window.onload event

--- analytics.php
+++ analytics.php
@@ -1,29 +1,31 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <script>
-    $.ajax({
-        type: 'POST',
-        url: './api.php',
-        datatype: 'json',
-        success: function (result) {
-            var ctx = document.getElementById("chart").getContext("2d");
-            var mychart = new Chart(ctx,
-            {
-                type: 'bar',
-                data: JSON.parse(result),
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    }
-                }
-            })
-        }
-    })
+    <script language='javascript'>
+    window.document.onload = function(e) {
+        $.ajax({
+            type: 'POST',
+            url: './api.php',
+            datatype: 'json',
+            success: function (result) {
+                var ctx = document.getElementById("chart").getContext("2d");
+                var mychart = new Chart(ctx,
+                                        {
+                                            type: 'bar',
+                                            data: JSON.parse(result),
+                                            options: {
+                                                scales: {
+                                                    yAxes: [{
+                                                        ticks: {
+                                                            beginAtZero: true
+                                                        }
+                                                    }]
+                                                }
+                                            }
+                                        })
+            }
+        })
+    }
     </script>
   </head>
 
