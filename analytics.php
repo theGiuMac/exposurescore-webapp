b174b9db87f629dbca5577a8eba5702aa3251cea
Giulio <maccarigiulio@pm.me>
2022-06-23 19:40:17 +0200

    Modified second graph's border in CSS

--- analytics.php
+++ analytics.php
@@ -3,76 +3,33 @@
   <head>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.8.0/dist/chart.min.js" integrity="sha256-cHVO4dqZfamRhWD7s4iXyaXWVK10odD+qp4xidFzqTI=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-
-    <style>
-      .container {
-        width: 100%;
-        max-width: 1174px;
-        min-width: 0;
-        background-color: #f1f1f1;
-        box-shadow: 0 1px 3px
-                    rgba(0,0,0,0,12), 0 1px 2px
-                    rgba(0,0,0,0,24);
-        margin: auto;
-        margin-top: auto;
-        margin-bottom: auto;
-        margin-left: auto;
-        margin-right: auto;
-        left: 0;
-        right: 0;
-        height: auto;
-        bottom: auto;
-      }
-
-     .firstgraph {
-         float: left;
-         width: 50%;
-         max-width: 485px;
-         min-width: 0;
-         background-color: transparent;
-         border: 5px solid green;
-         padding: 20px;
-     }
-     
-     .secondgraph {
-         float: right;
-         width: 50%;
-         max-width: 485px;
-         min-width: 0;
-         background-color: transparent;
-         border: 5px solid orange;
-         padding: 20px;
-     }
-    </style>
-
+    <script type="text/javascript" src="./bargraph.js"></script>
+    <link rel="stylesheet" type="text/css" href="graph_styles.css">
   </head>
 
   <body>
-    <script type="text/javascript" src="./bargraph.js"></script>
-
     <h3>Analytics section</h3>
-
     <div id="container" class="container" style="display: block;">
-
+      <aside>
+        <div id="search">
+          <input id="btnSearch" type="button" value="Search" />
+          <label for="txtSearch">Search</label>
+          <input type="text" name="txtSearch" id="txtSearch" />
+        </div>
+      </aside>
       <div class="firstgraph">
         <!-- Interactive graph for the browser identifiers and normalized exposure score -->
         <canvas id="chart"></canvas>
       </div>
-
       <div class="secondgraph">
         <canvas id=""></canvas>
       </div>
-
       <div class="thirdgraph">
 
-
       </div>
-
       <div class="fourthgraph">
 
-
       </div>
-     
     </div>
   </body>
 </html>
