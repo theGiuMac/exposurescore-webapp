bc396bdb1e6cc166f58431140653ce4fced5dc62
Giulio <maccarigiulio@pm.me>
2022-07-04 18:28:15 +0200

    Added counter

--- index.php
+++ index.php
@@ -171,10 +171,11 @@
         </div>
     </div>
 
-    <!-- hitwebcounter Code START -->
-    <a href="https://www.hitwebcounter.com" target="_blank">
-      <img src="https://hitwebcounter.com/counter/counter.php?page=8010472&style=0005&nbdigits=5&type=page&initCount=0" title="Free Counter" Alt="web counter"   border="0" /></a>              
+    <div id="sfcfaah9bke1fr1pm1m2a7jhf6mblh18541"></div>
+    <script type="text/javascript" src="https://counter9.stat.ovh/private/counter.js?c=faah9bke1fr1pm1m2a7jhf6mblh18541&down=async" async></script>
+    <noscript><a href="https://www.freecounterstat.com" title="web counter"><img src="https://counter9.stat.ovh/private/freecounterstat.php?c=faah9bke1fr1pm1m2a7jhf6mblh18541" border="0" title="web counter" alt="web counter"></a></noscript>
     
+
     <ul style="margin-top: 100px; margin-bottom: 25px; text-align: center;">
         <a class="nav-link active" aria-current="page" href="./analytics.php">
             <h3 style="color:black;"><b>Analytics</b></h3>
